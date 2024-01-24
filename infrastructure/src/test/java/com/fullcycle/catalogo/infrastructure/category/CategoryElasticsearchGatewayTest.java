6485d35ce66c064816be49235260a02c104ac6ee
Gustavo Queiroz <gustavo.qmb@gmail.com>
2024-01-23 22:30:04 -0300

    Testes integrados do método delete do gateway de categoria

--- infrastructure/src/test/java/com/fullcycle/catalogo/infrastructure/category/CategoryElasticsearchGatewayTest.java
+++ infrastructure/src/test/java/com/fullcycle/catalogo/infrastructure/category/CategoryElasticsearchGatewayTest.java
@@ -2,6 +2,7 @@ package com.fullcycle.catalogo.infrastructure.category;
 
 import com.fullcycle.catalogo.AbstractElasticsearchTest;
 import com.fullcycle.catalogo.domain.Fixture;
+import com.fullcycle.catalogo.infrastructure.category.persistence.CategoryDocument;
 import com.fullcycle.catalogo.infrastructure.category.persistence.CategoryRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -40,5 +41,31 @@ public class CategoryElasticsearchGatewayTest extends AbstractElasticsearchTest 
         Assertions.assertEquals(aulas.createdAt(), actualCategory.createdAt());
         Assertions.assertEquals(aulas.updatedAt(), actualCategory.updatedAt());
         Assertions.assertEquals(aulas.deletedAt(), actualCategory.deletedAt());
+    }
+
+    @Test
+    public void givenValidId_whenCallsDeleteById_shouldDeleteIt() {
+        // given
+        final var aulas = Fixture.Categories.aulas();
+
+        this.categoryRepository.save(CategoryDocument.from(aulas));
+
+        final var expectedId = aulas.id();
+        Assertions.assertTrue(this.categoryRepository.existsById(expectedId));
+
+        // when
+        this.categoryGateway.deleteById(expectedId);
+
+        // then
+        Assertions.assertFalse(this.categoryRepository.existsById(expectedId));
+    }
+
+    @Test
+    public void givenInvalidId_whenCallsDeleteById_shouldBeOk() {
+        // given
+        final var expectedId = "any";
+
+        // when/then
+        Assertions.assertDoesNotThrow(() -> this.categoryGateway.deleteById(expectedId));
     }
 }
