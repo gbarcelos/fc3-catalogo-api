9de156c39bd0260f3baad49e251cc8f2980cad3f
Gustavo Queiroz <gustavo.qmb@gmail.com>
2024-02-20 18:21:14 -0300

    Testes integrados do kafka listener de categoria

--- infrastructure/src/test/java/com/fullcycle/catalogo/AbstractEmbeddedKafkaTest.java
+++ infrastructure/src/test/java/com/fullcycle/catalogo/AbstractEmbeddedKafkaTest.java
@@ -1,6 +1,9 @@
 package com.fullcycle.catalogo;
 
 import com.fullcycle.catalogo.infrastructure.configuration.WebServerConfig;
+import com.fullcycle.catalogo.infrastructure.kafka.models.connect.Source;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.junit.jupiter.api.AfterAll;
@@ -17,6 +20,8 @@ import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.kafka.test.utils.KafkaTestUtils;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.Collections;
+
 @EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:9092", "port=9092"})
 @ActiveProfiles("test-integration")
 @EnableAutoConfiguration(exclude = {
@@ -32,11 +37,15 @@ public abstract class AbstractEmbeddedKafkaTest {
 
     private Producer<String, String> producer;
 
+    private AdminClient admin;
+
     @Autowired
     protected EmbeddedKafkaBroker kafkaBroker;
 
     @BeforeAll
     void init() {
+        admin = AdminClient.create(Collections.singletonMap(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaBroker.getBrokersAsString()));
+
         producer =
                 new DefaultKafkaProducerFactory<>(KafkaTestUtils.producerProps(kafkaBroker), new StringSerializer(), new StringSerializer())
                         .createProducer();
@@ -47,7 +56,15 @@ public abstract class AbstractEmbeddedKafkaTest {
         producer.close();
     }
 
+    protected AdminClient admin() {
+        return admin;
+    }
+
     protected Producer<String, String> producer() {
         return producer;
+    }
+
+    protected Source aSource() {
+        return new Source("admin_mysql", "admin_catalogo", "categories");
     }
 }
