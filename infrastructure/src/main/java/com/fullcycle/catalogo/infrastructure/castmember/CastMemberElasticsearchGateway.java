fe6c49be87f3cba6cc5de59d98458a098249c61e
Gustavo Queiroz <gustavo.qmb@gmail.com>
2024-08-28 08:01:01 -0300

    Criando o elasticsearch repository e gateway de cast member

---
+++ infrastructure/src/main/java/com/fullcycle/catalogo/infrastructure/castmember/CastMemberElasticsearchGateway.java
@@ -0,0 +1,48 @@
+package com.fullcycle.catalogo.infrastructure.castmember;
+
+import com.fullcycle.catalogo.domain.castmember.CastMember;
+import com.fullcycle.catalogo.domain.castmember.CastMemberGateway;
+import com.fullcycle.catalogo.domain.castmember.CastMemberSearchQuery;
+import com.fullcycle.catalogo.domain.pagination.Pagination;
+import com.fullcycle.catalogo.infrastructure.castmember.persistence.CastMemberRepository;
+import java.util.Objects;
+import java.util.Optional;
+import org.springframework.data.elasticsearch.core.SearchOperations;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CastMemberElasticsearchGateway implements CastMemberGateway {
+
+  private static final String NAME_PROP = "name";
+  private static final String KEYWORD = ".keyword";
+
+  private final CastMemberRepository castMemberRepository;
+  private final SearchOperations searchOperations;
+
+  public CastMemberElasticsearchGateway(
+      final CastMemberRepository castMemberRepository,
+      final SearchOperations searchOperations
+  ) {
+    this.castMemberRepository = Objects.requireNonNull(castMemberRepository);
+    this.searchOperations = Objects.requireNonNull(searchOperations);
+  }
+
+  @Override
+  public CastMember save(final CastMember aMember) {
+    return null;
+  }
+
+  @Override
+  public void deleteById(final String anId) {
+  }
+
+  @Override
+  public Optional<CastMember> findById(final String anId) {
+    return Optional.empty();
+  }
+
+  @Override
+  public Pagination<CastMember> findAll(final CastMemberSearchQuery aQuery) {
+    return null;
+  }
+}
