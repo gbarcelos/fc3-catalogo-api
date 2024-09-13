f362f0ded5e36a06d65faf2f2ecfe4b5225de235
Gustavo Queiroz <gustavo.qmb@gmail.com>
2024-09-13 18:28:04 -0300

    Configurando os beans dos usecases de genre

---
+++ infrastructure/src/main/java/com/fullcycle/catalogo/infrastructure/configuration/usecases/GenreUseCasesConfig.java
@@ -0,0 +1,34 @@
+package com.fullcycle.catalogo.infrastructure.configuration.usecases;
+
+import com.fullcycle.catalogo.application.genre.delete.DeleteGenreUseCase;
+import com.fullcycle.catalogo.application.genre.list.ListGenreUseCase;
+import com.fullcycle.catalogo.application.genre.save.SaveGenreUseCase;
+import com.fullcycle.catalogo.domain.genre.GenreGateway;
+import java.util.Objects;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration(proxyBeanMethods = false)
+public class GenreUseCasesConfig {
+
+  private final GenreGateway genreGateway;
+
+  public GenreUseCasesConfig(final GenreGateway genreGateway) {
+    this.genreGateway = Objects.requireNonNull(genreGateway);
+  }
+
+  @Bean
+  SaveGenreUseCase saveGenreUseCase() {
+    return new SaveGenreUseCase(genreGateway);
+  }
+
+  @Bean
+  DeleteGenreUseCase deleteGenreUseCase() {
+    return new DeleteGenreUseCase(genreGateway);
+  }
+
+  @Bean
+  ListGenreUseCase listGenreUseCase() {
+    return new ListGenreUseCase(genreGateway);
+  }
+}
