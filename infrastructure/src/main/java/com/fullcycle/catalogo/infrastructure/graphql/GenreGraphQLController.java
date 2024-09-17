307cfbb0304b9a4b508154673b23426d925a0231
Gustavo Queiroz <gustavo.qmb@gmail.com>
2024-09-17 16:08:06 -0300

    Implementando a mutation de salvar genre

--- infrastructure/src/main/java/com/fullcycle/catalogo/infrastructure/graphql/GenreGraphQLController.java
+++ infrastructure/src/main/java/com/fullcycle/catalogo/infrastructure/graphql/GenreGraphQLController.java
@@ -2,10 +2,12 @@ package com.fullcycle.catalogo.infrastructure.graphql;
 
 import com.fullcycle.catalogo.application.genre.list.ListGenreUseCase;
 import com.fullcycle.catalogo.application.genre.save.SaveGenreUseCase;
+import com.fullcycle.catalogo.infrastructure.genre.models.GenreDTO;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -13,9 +15,12 @@ import org.springframework.stereotype.Controller;
 public class GenreGraphQLController {
 
   private final ListGenreUseCase listGenreUseCase;
+  private final SaveGenreUseCase saveGenreUseCase;
 
-  public GenreGraphQLController(final ListGenreUseCase listGenreUseCase, final SaveGenreUseCase saveGenreUseCase) {
+  public GenreGraphQLController(final ListGenreUseCase listGenreUseCase,
+      final SaveGenreUseCase saveGenreUseCase) {
     this.listGenreUseCase = Objects.requireNonNull(listGenreUseCase);
+    this.saveGenreUseCase = Objects.requireNonNull(saveGenreUseCase);
   }
 
   @QueryMapping
@@ -27,7 +32,22 @@ public class GenreGraphQLController {
       @Argument final String direction,
       @Argument final Set<String> categories
   ) {
-    final var input = new ListGenreUseCase.Input(page, perPage, search, sort, direction, categories);
+    final var input = new ListGenreUseCase.Input(page, perPage, search, sort, direction,
+        categories);
     return this.listGenreUseCase.execute(input).data();
+  }
+
+  @MutationMapping
+  public SaveGenreUseCase.Output saveGenre(@Argument(name = "input") final GenreDTO arg) {
+    final var input =
+        new SaveGenreUseCase.Input(arg.id(),
+            arg.name(),
+            arg.active(),
+            arg.categories(),
+            arg.createdAt(),
+            arg.updatedAt(),
+            arg.deletedAt());
+
+    return this.saveGenreUseCase.execute(input);
   }
 }
